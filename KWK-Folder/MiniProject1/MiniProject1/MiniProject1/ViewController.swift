11c6bc1f2e6b448dc1a0d25a38f9f8e2620fcd27
VivanKwong <68553795+VivanKwong@users.noreply.github.com>
2021-07-28 14:16:21 -0400

    2nd Commit

---
+++ KWK-Folder/MiniProject1/MiniProject1/MiniProject1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MiniProject1
+//
+//  Created by Scholar on 7/28/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
