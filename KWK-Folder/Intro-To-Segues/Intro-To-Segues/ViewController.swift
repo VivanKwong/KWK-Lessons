7c7f3ef4ad6f16c80aa92d030d41d22443aa657d
VivanKwong <68553795+VivanKwong@users.noreply.github.com>
2021-07-29 12:09:31 -0400

    Add files via upload

---
+++ KWK-Folder/Intro-To-Segues/Intro-To-Segues/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Intro-To-Segues
+//
+//  Created by Scholar on 7/29/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func button1to2(_ sender: UIButton) {
+    }
+}
+
