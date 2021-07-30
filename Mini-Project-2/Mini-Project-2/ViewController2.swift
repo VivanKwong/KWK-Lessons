91c0dea6c2e3e6d187e3aa12131d47dd009f4fb5
VivanKwong <68553795+VivanKwong@users.noreply.github.com>
2021-07-30 10:33:23 -0400

    Add files via upload

---
+++ Mini-Project-2/Mini-Project-2/ViewController2.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController2.swift
+//  Mini-Project-2
+//
+//  Created by Scholar on 7/29/21.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+    @IBOutlet weak var answer2: UILabel!
+    
+    @IBOutlet weak var imageX2: UIImageView!
+    @IBOutlet weak var image2: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func choiceA2(_ sender: UIButton) {
+    }
+    
+    @IBAction func choiceB2(_ sender: UIButton) {
+    }
+    
+    @IBAction func choiceC2(_ sender: UIButton) {
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Mini-Project-2/Mini-Project-2/ViewController3.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController3.swift
+//  Mini-Project-2
+//
+//  Created by Scholar on 7/29/21.
+//
+
+import UIKit
+
+class ViewController3: UIViewController {
+    @IBOutlet weak var answer3: UILabel!
+    
+    @IBOutlet weak var imageX3: UIImageView!
+    @IBOutlet weak var image3: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func choiceA3(_ sender: UIButton) {
+    }
+    
+    @IBAction func choiceB3(_ sender: UIButton) {
+    }
+    
+    @IBAction func choiceC3(_ sender: UIButton) {
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
